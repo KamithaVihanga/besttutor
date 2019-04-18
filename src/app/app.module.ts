176150b97f8ee11e76b56a37bf1a6e0ec0bf9f4d
Kamitha Bodhinayake <kvihanga@gmail.com>
2019-04-19 00:05:03 +0530

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,48 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignInComponent } from './sign-in/sign-in.component';
+import {RouterModule, Routes} from "@angular/router";
+import { SignUpComponent } from './sign-up/sign-up.component';
+import { MainComponent } from './main/main.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
+import { FindComponent } from './find/find.component';
+import { AdminComponent } from './admin/admin.component';
 
+const routes: Routes = [{
+  path:'',
+  redirectTo:'/main',
+  pathMatch:'full'
+},{
+  path:"main",
+  component: MainComponent
+},{
+  path:"sign-in",
+  component: SignInComponent
+},{
+  path:"sign-up",
+  component:SignUpComponent
+},
+  {
+    path:"find",
+    component: FindComponent
+  }];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignInComponent,
+    SignUpComponent,
+    MainComponent,
+    FooterComponent,
+    NavComponent,
+    FindComponent,
+    AdminComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    // AppRoutingModule
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
